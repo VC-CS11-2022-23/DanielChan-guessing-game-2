cc5b76c1eb36000c19efd09bfa28c061476e41f3
gaping-gorilla15 <119447909+gaping-gorilla15@users.noreply.github.com>
2022-12-01 17:46:56 +0000

    done

--- boilerplate.py
+++ boilerplate.py
@@ -7,9 +7,23 @@ import random
 number = random.randint(1, 10)
 
 TRIES = 0
+if TRIES > 3:
+    print("you lose")
+    
 
 # GAME LOOP
 while True:
     guess = int(input("Guess a number between 1 and 10: "))
+    if guess == number:
+        print("correct")
+        break
+        
+
+    if guess > number:
+        print("too high")
+        
+    else:
+        print("Too low")
+        break
 
     # implement your logic below
